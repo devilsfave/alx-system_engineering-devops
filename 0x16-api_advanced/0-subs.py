70bfef2a65d49e9edbef5724ae4b314f86d07a6a
devilsfave <132396927+devilsfave@users.noreply.github.com>
2024-04-10 05:40:18 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,17 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of subscribers
-for a given subreddit
-"""
-
-import requests
-from sys import argv
-import json
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit
-    Args:
-        subreddit (str): the name of the subreddit
-    Returns:
-        int: the number of subscribers, or 0 if the subreddit is invalid
-    """
-    subreddit = "programming"
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': '0-subs/1.0 (by /u/yaasgyan)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    print("Response status code:", response.status_code)
-    # data = response.json()
-    if response.status_code >= 300:
-        return (0)
-    data = response.json()
-    data_dict = data.get('data', {}).get('subscribers', 0)
-    return(data_dict)
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
 
-if __name__ == "__main_":
-    subscribers = number_of_subscribers()
-    print(subscribers)
+    return sub_info.json().get("data").get("subscribers")
