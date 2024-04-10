a64324e1c506e747a66193b7b1b5e73d7fd951a9
devilsfave <132396927+devilsfave@users.noreply.github.com>
2024-04-10 05:44:59 +0000

    Update 0-main.py

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -2,11 +2,14 @@
 """
 0-main
 """
+
 import sys
+import requests  # Import requests library
 
 if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        subreddit = sys.argv[1]
+        subscribers = __import__('0-subs').number_of_subscribers(subreddit)
+        print("{:d}".format(subscribers))
