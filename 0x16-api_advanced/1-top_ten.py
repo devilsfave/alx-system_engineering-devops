4317c9667c44cf4bb1eb8ef6771bfbc2b9592a09
devilsfave <132396927+devilsfave@users.noreply.github.com>
2024-04-10 05:42:44 +0000

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and print the titles of the first 10
-hot posts listed for a given subreddit
-"""
-
-import requests
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit
-    Args:
-        subreddit (str): the name of the subreddit
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'queenbae'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
 
-    if response.status_code != 200:
-        print(None)
-        return
-
-    data = response.json()
-
-    if 'data' not in data or 'children' not in data['data']:
-        print(None)
-        return
-
-    posts = data['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
